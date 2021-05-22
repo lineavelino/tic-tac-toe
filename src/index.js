c27c5a2438a26935277add8ea254c47a0f08bc10
Aline Avelino <aline.savelino@gmail.com>
2021-05-22 20:57:44 -0300

    Criação componente Game

--- src/index.js
+++ src/index.js
@@ -42,7 +42,18 @@ class Board extends React.Component {
     }
 }
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class Game extends React.Component {
+    render() {
+        return (
+            <div className="game">
+                <div className="game-board">
+                    <Board />
+                </div>
+                <div className="game-info">
+                    <div>{/* status */}</div>
+                    <ol>{/* todo */}</ol>
+                </div>
+            </div>
+        );
+    }
+}
